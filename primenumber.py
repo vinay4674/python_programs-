92b0e109ce19b91d28ab0d6d72d4f1f4061e4cf6
vinay4674 <bobbalavinaykumar24@gmail.com>
2022-07-22 11:28:47 +0530

    this is my second commit

--- primenumber.py
+++ primenumber.py
@@ -1,19 +1,19 @@
 # This program used to find prime numbers  
 
-num=int(input("Enter the number to check prime or not:" )
+num=int(input("Enter the number to check prime or not:" ))
 
 if(num>1):
 	
 	for n in range(2,num):
-		if (num%n)==0;
-		print("it is not a prime number")
-		break
+		if (num%n)==0:
+		   print("it is not a prime number")
+		   break
 	
 	else:
 		print("it is a prime number")
 
 
 
-	else:
+else:
 
 	print("it is not a prime number")
